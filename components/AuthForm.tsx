2e8ba87db894bf0b4ee2937df7dd4a907018e745
shardul dhanokar <dhanokarshardul@gmail.com>
2025-03-26 22:31:30 +0530

    implement auth and storage

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,29 +1,37 @@
-"use client"
+"use client";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
+import { z } from "zod";
+import Link from "next/link";
+import Image from "next/image";
+import { toast } from "sonner";
+import { auth } from "@/firebase/client";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-import { Button } from "@/components/ui/button"
-import { Form } from "@/components/ui/form"
-import Image from "next/image"
-import Link from "next/link"
-import { toast } from "sonner"
-import FormField from "./FormField"
-import { useRouter } from "next/navigation"
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+
+import { Form } from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+
+import { signIn, signUp } from "@/lib/actions/auth.action";
+import FormField from "./FormField";
 
 const authFormSchema = (type: FormType) => {
   return z.object({
     name: type === "sign-up" ? z.string().min(3) : z.string().optional(),
     email: z.string().email(),
     password: z.string().min(3),
-  })
-}
+  });
+};
 
 const AuthForm = ({ type }: { type: FormType }) => {
   const router = useRouter();
-  const formSchema = authFormSchema(type);
 
+  const formSchema = authFormSchema(type);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -31,30 +39,64 @@ const AuthForm = ({ type }: { type: FormType }) => {
       email: "",
       password: "",
     },
-  })
+  });
 
-  // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    try{
-      if(type === 'sign-up'){
-        toast.success('Account created successfully! Please Sign In');
-        router.push('/sign-in')
-        console.log('SIGN UP', values);
-      }else{
-        toast.success('Sign In successfully');
-        router.push('/')
-        console.log('SIGN IN', values);
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    try {
+      if (type === "sign-up") {
+        const { name, email, password } = data;
+
+        const userCredential = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+
+        const result = await signUp({
+          uid: userCredential.user.uid,
+          name: name!,
+          email,
+          password,
+        });
+
+        if (!result.success) {
+          toast.error(result.message);
+          return;
+        }
+
+        toast.success("Account created successfully. Please sign in.");
+        router.push("/sign-in");
+      } else {
+        const { email, password } = data;
+
+        const userCredential = await signInWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+
+        const idToken = await userCredential.user.getIdToken();
+        if (!idToken) {
+          toast.error("Sign in Failed. Please try again.");
+          return;
+        }
+
+        await signIn({
+          email,
+          idToken,
+        });
+
+        toast.success("Signed in successfully.");
+        
+        router.push("/");
       }
-    }
-    catch(error){
+    } catch (error) {
       console.log(error);
-      toast.error(`There was an error: ${error}`);
+      toast.error(`There was an error: ${error}`)
     }
-  }
+  };
 
-  const isSignIn = type === 'sign-in';
+  const isSignIn = type === "sign-in";
 
   return (
     <div className="card-border lg:min-w-[566px]">
@@ -63,45 +105,58 @@ const AuthForm = ({ type }: { type: FormType }) => {
           <Image src="/logo.svg" alt="logo" height={32} width={38} />
           <h2 className="text-primary-100">PrepWise</h2>
         </div>
+
         <h3>Practice job interviews with AI</h3>
 
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6 mt-4 form">
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="w-full space-y-6 mt-4 form"
+          >
             {!isSignIn && (
-              <FormField 
-              control={form.control} 
-              name="name" 
-              label="name" 
-              placeholder="Your Name"
+              <FormField
+                control={form.control}
+                name="name"
+                label="Name"
+                placeholder="Your Name"
+                type="text"
               />
             )}
-            <FormField 
-              control={form.control} 
-              name="email" 
-              label="Email" 
-              placeholder="Your email"
-              type="email"
-              />
-              <FormField 
-              control={form.control} 
-              name="password" 
-              label="Password" 
-              placeholder="Enter Password"
-              type="password"
-              />
 
-            <Button className="btn" type="submit">{isSignIn ? 'Sign in ' : 'Create an Account'}</Button>
+            <FormField
+              control={form.control}
+              name="email"
+              label="Email"
+              placeholder="Your email address"
+              type="email"
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              label="Password"
+              placeholder="Enter your password"
+              type="password"
+            />
+
+            <Button className="btn" type="submit">
+              {isSignIn ? "Sign In" : "Create an Account"}
+            </Button>
           </form>
         </Form>
 
         <p className="text-center">
-          {isSignIn ? 'No account yet? ' : 'Already have an account? '}
-          <Link href={!isSignIn ? '/sign-in' : '/sign-up'} className="font-bold text-user-primary ml-1" >{!isSignIn ? "Sign in" : "Sign up"}</Link>
+          {isSignIn ? "No account yet?" : "Have an account already?"}
+          <Link
+            href={!isSignIn ? "/sign-in" : "/sign-up"}
+            className="font-bold text-user-primary ml-1"
+          >
+            {!isSignIn ? "Sign In" : "Sign Up"}
+          </Link>
         </p>
-
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AuthForm
+export default AuthForm;
---
+++ firebase/client.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBZzCvxXsCPiO96ygPSwafK5ZzgGayX2-4",
+  authDomain: "prepwise-c21f5.firebaseapp.com",
+  projectId: "prepwise-c21f5",
+  storageBucket: "prepwise-c21f5.firebasestorage.app",
+  messagingSenderId: "17184466447",
+  appId: "1:17184466447:web:d38e21d3f5b7f3db41257a",
+  measurementId: "G-3W23W2C1YR"
+};
+
+// Initialize Firebase
+const app = !getApps.length ? initializeApp(firebaseConfig): getApp();
+
+export const auth = getAuth(app);
+export const db= getFirestore(app)
